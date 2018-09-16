74d621213c33833f5c4ecea72e4fc3779345c705
Artemiy Knipe | Артемий Кондатьев <artemiy.kondratiev@gmail.com>
2018-09-16 15:51:08 +0100

    Sketch members-service (#12)

---
+++ services/members-service/application.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+
+application = Flask(__name__)
+
+
+@application.route('/members', methods=['GET', 'POST'])
+def members():
+    if request.method == 'GET':
+        return jsonify([{ 'id': 1, 'name': 'Olga Rios' }, { 'id': 2, 'name': 'Mitchell Simpson' }]), 200
+    elif request.method == 'POST':
+        return jsonify({ 'id': 1, 'name': 'Olga Rios' }), 201
+
+
+@application.route('/members/<int:member_id>', methods=['PUT', 'DELETE'])
+def member(member_id):
+    if request.method == 'PUT':
+        return jsonify({ 'id': member_id, 'name': 'Gwendolyn Carlson' }), 200
+    elif request.method == 'DELETE':
+        return jsonify(None), 204
+
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run(host='0.0.0.0', port='3000')
