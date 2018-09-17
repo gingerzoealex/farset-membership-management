3fec40c4ef374a9b980f1d937541b42fbb83594e
Art Knipe <art@shopkeep.com>
2018-09-18 00:52:40 +0100

    member-management-client: Add ability to update member

--- services/member-management-client/src/members-service.js
+++ services/member-management-client/src/members-service.js
@@ -29,6 +29,22 @@ export function createMember({ name }) {
   );
 }
 
+export function updateMember(id, { name }) {
+  return fetch(`${SERVICE_URL}/members/${id}`, {
+    mode: 'cors',
+    method: 'put',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }),
+    body: JSON.stringify({
+      name
+    })
+  }).then(
+    (response) => response.json()
+  );
+}
+
 export function deleteMember(id) {
   return fetch(`${SERVICE_URL}/members/${id}`, {
     mode: 'cors',
