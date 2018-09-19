fd80d435006f90a734ad644c690098c327a8ad22
Art Knipe <art@shopkeep.com>
2018-09-19 01:54:15 +0100

    members-service: Add database integration with PostgreSQL

--- services/members-service/application.py
+++ services/members-service/application.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 
 application = Flask(__name__)
@@ -8,19 +9,60 @@ application = Flask(__name__)
 CORS(application)
 
 
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:@localhost:5432/members'
+
+
+database = SQLAlchemy(application)
+
+class Member(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String)
+
+    def __repr__(self):
+        return '<Member(id=%i name=%s)>' % (self.id, self.name)
+
+    def serialize(self):
+        return { 'id': self.id, 'name': self.name }
+
+database.create_all()
+
+
 @application.route('/members', methods=['GET', 'POST'])
 def members():
     if request.method == 'GET':
-        return jsonify([{ 'id': 1, 'name': 'Olga Rios' }, { 'id': 2, 'name': 'Mitchell Simpson' }]), 200
+        members = Member.query.all()
+
+        return jsonify([member.serialize() for member in members]), 200
     elif request.method == 'POST':
-        return jsonify({ 'id': 1, 'name': 'Olga Rios' }), 201
+        name = request.json.get('name', 'No name')
+
+        member = Member(name=name)
+
+        database.session.add(member)
+        database.session.commit()
+
+        return jsonify(member.serialize()), 201
 
 
 @application.route('/members/<int:member_id>', methods=['PUT', 'DELETE'])
 def member(member_id):
     if request.method == 'PUT':
-        return jsonify({ 'id': member_id, 'name': 'Gwendolyn Carlson' }), 200
+        name = request.json.get('name', 'No name')
+
+        member = Member.query.get(member_id)
+
+        member.name = name
+
+        database.session.add(member)
+        database.session.commit()
+
+        return jsonify(member.serialize()), 200
     elif request.method == 'DELETE':
+        member = Member.query.get(member_id)
+
+        database.session.delete(member)
+        database.session.commit()
+
         return jsonify(None), 204
 
 
